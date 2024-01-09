350c68af5376253289953a316bf5f13489bcae43
Abhijith J <abhijithj47s0@gmail.com>
2024-01-09 13:57:45 +0530

    initial server-setup

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+const tickets = require("./routes/tickets");
+const CustomError = require("./Utils/customError");
+const errorHandler = require("./Controllers/errorController");
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use("/api/tickets", tickets);
+
+app.all("*", (req, res, next) => {
+  const err = new CustomError(
+    `Can't find ${req.originalUrl} on the server!`,
+    404
+  );
+  next(err);
+});
+app.use(errorHandler);
+
+
+module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,17 @@
+const dotEnv = require("dotenv");
+dotEnv.config();
+ 
+const app = require("./app");
+const client = require("./db/config");
+
+client.connect().then(()=>{
+console.log('DB Connection successful');
+}).catch((err)=>{
+console.log(err.message);
+})
+
+const PORT=process.env.PORT || 3500
+app.listen(PORT,()=>{
+console.log(`Server listening at PORT:${PORT}`);
+})
+ 
